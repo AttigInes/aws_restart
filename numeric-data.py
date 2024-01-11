5461eb11149dc22d3de0c276b330abda889e517d
AttigInes <156218950+AttigInes@users.noreply.github.com>
2024-01-11 02:56:23 -0800

    Add files via upload

---
+++ numeric-data.py
@@ -0,0 +1,20 @@
+print("Python has three numeric types: int, float, and complex")
+MyValue =1
+print(MyValue)
+print(type(MyValue))
+print(str(MyValue) + " is of the data type " + str(type(MyValue)))
+
+myValue =3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is the data type " + str(type(myValue)))
+
+myvalue =5j
+print(myvalue)
+print(type(myvalue))
+print(str(myvalue) + " is the data type " + str(type(myvalue)))
+
+MyValue2 =True
+print(MyValue2)
+print(type(MyValue2))
+print(str(MyValue2)+ " is the data type " + str(type(MyValue2)))
---
+++ string-data-type.py
@@ -0,0 +1,19 @@
+
+
+myFruitList= ["apple","banana","cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2]= "orange"
+print(myFruitList)
+
+MyFinalAnswerTuple= ("apple","banana","pineapple")
+print(MyFinalAnswerTuple)
+print(type(MyFinalAnswerTuple))
+print(MyFinalAnswerTuple[0])
+print(MyFinalAnswerTuple[1])
+print(MyFinalAnswerTuple[2])
